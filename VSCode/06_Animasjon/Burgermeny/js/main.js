38e7c2eabac2b0e86ea5b890b9fe54a7bdfbb019
Mads Opstad Reistadbakk <mareis@Madss-MacBook-Pro.local>
2019-12-12 18:10:17 +0100

    ,

--- VSCode/06_Animasjon/Burgermeny/js/main.js
+++ VSCode/06_Animasjon/Burgermeny/js/main.js
@@ -1,40 +1,25 @@
 let burger_El = document.querySelector("#burger_svg");
 let meny_El = document.querySelector("#meny");
-meny_El.style.display = "none";
+
+function reset() {
+  meny_El.style.display = "none";
+  gsap.set("#meny", { opacity: 1, x: 0 });
+}
+
+reset();
+
 burger_El.addEventListener("click", function() {
   let visning = meny_El.style.display;
   if (visning == "none") {
     meny_El.style.display = "block";
-    gsap.to("#Line_1", {
-      rotation: 43,
-      duration: 1
-    });
-    gsap.to("#Line_2", {
-      opacity: 0,
-      duration: 1
-    });
-    gsap.to("#Line_3", {
-      rotation: -43,
-      duration: 1
-    });
-    gsap.from("#meny", {
-      opacity: 0,
-      duration: 0.5,
-      x: 300
-    });
+    gsap.to("#Line_1", { rotation: 43, duration: 0.8 });
+    gsap.to("#Line_2", { opacity: 0, duration: 0.5 });
+    gsap.to("#Line_3", { rotation: -43, duration: 0.8 });
+    gsap.from("#meny", { opacity: 0, duration: 0.5, x: 300 });
   } else {
-    meny_El.style.display = "none";
-    gsap.to("#Line_1", {
-      rotation: 0,
-      duration: 1
-    });
-    gsap.to("#Line_2", {
-      opacity: 1,
-      duration: 1
-    });
-    gsap.to("#Line_3", {
-      rotation: 0,
-      duration: 1
-    });
+    gsap.to("#Line_1", { rotation: 0, duration: 0.8 });
+    gsap.to("#Line_2", { opacity: 1, duration: 0.5 });
+    gsap.to("#Line_3", { rotation: 0, duration: 0.8 });
+    gsap.to("#meny", { opacity: 0, duration: 0.5, x: 300, onComplete: reset });
   }
 });
