34901626b5b6b0c17a23a12a7d057cc43a544476
Mads Opstad Reistadbakk <mareis@Madss-MacBook-Pro.local>
2019-12-12 20:22:50 +0100

    ,

--- VSCode/06_Animasjon/Burgermeny/js/main.js
+++ VSCode/06_Animasjon/Burgermeny/js/main.js
@@ -1,9 +1,13 @@
 let burger_El = document.querySelector("#burger_svg");
 let meny_El = document.querySelector("#meny");
 
+gsap.from("#logo", { x: -100, duration: 0.5 });
+gsap.from("#burger_svg", { x: 100, duration: 0.5 });
+gsap.from("main", { opacity: 0, duration: 1 });
+
 function reset() {
   meny_El.style.display = "none";
-  gsap.set("#meny", { opacity: 1, x: 0 });
+  gsap.set("#meny", { opacity: 0.9, x: 0 });
 }
 
 reset();
@@ -15,11 +19,11 @@ burger_El.addEventListener("click", function() {
     gsap.to("#Line_1", { rotation: 43, duration: 0.8 });
     gsap.to("#Line_2", { opacity: 0, duration: 0.5 });
     gsap.to("#Line_3", { rotation: -43, duration: 0.8 });
-    gsap.from("#meny", { opacity: 0, duration: 0.5, x: 300 });
+    gsap.from("#meny", { opacity: 0, duration: 0.4, x: 200 });
   } else {
     gsap.to("#Line_1", { rotation: 0, duration: 0.8 });
     gsap.to("#Line_2", { opacity: 1, duration: 0.5 });
     gsap.to("#Line_3", { rotation: 0, duration: 0.8 });
-    gsap.to("#meny", { opacity: 0, duration: 0.5, x: 300, onComplete: reset });
+    gsap.to("#meny", { opacity: 0, duration: 0.4, x: 300, onComplete: reset });
   }
 });
